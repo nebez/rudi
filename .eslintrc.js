1a0696fae3939206817f08793f2a2eca75b617a0
Nebez Briefkani <me@nebezb.com>
2019-12-24 15:59:57 -0500

    Add spec file to test interfaces before implementation

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 require("@rushstack/eslint-config/patch-eslint6");
 
 module.exports = {
+  ignorePatterns: ["test/spec.ts", "node_modules/"],
   extends: [
     "@rushstack/eslint-config",
     "plugin:prettier/recommended",
