b008c10c8fc61de8a6f3a72baae7a7f351b62824
Nebez Briefkani <me@nebezb.com>
2019-12-24 14:19:37 -0500

    Add prettier to rushstack linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 require("@rushstack/eslint-config/patch-eslint6");
 
 module.exports = {
-  extends: ["@rushstack/eslint-config"],
+  extends: [
+    "@rushstack/eslint-config",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: { tsconfigRootDir: __dirname },
 };
